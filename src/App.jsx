3e2865c14c6e348957cf17c65612ae0ffb4a2a18
Waheed Khan <wahidXX@xxx.com>
2026-01-13 03:53:26 +0300

    Added tailwindcss to reactjs

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="min-h-screen bg-blue-100 flex items-center justify-center">
+      <div className="bg-white p-8 rounded-lg shadow-lg">
+        <h2 className="text-2xl font-bold mb-6">Welcome to Product Zone</h2>
+        <p className="text-gray-700">This is a sample application using React and Tailwind CSS.</p>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
