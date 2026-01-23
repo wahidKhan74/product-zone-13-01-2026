8c6c5db7452e49ef29cfdffdc7273af90f4d5d08
Waheed Khan <wahidXX@xxx.com>
2026-01-23 04:24:33 +0300

    Added mock api with json server

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -4,6 +4,8 @@ import cartReducer from "./cartReducer";
 // 3. combine reducers for the store
 const rootReducer = combineReducers({
     cart: cartReducer,
+    user: (state = {}, action) => state, // placeholder for user reducer
+    emp : (state = [], action) => state, // placeholder for emp reducer
 });
 
 export default rootReducer;
