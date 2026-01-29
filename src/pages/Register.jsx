454190d4e50a13228f357330722c14eac8f71d3d
Waheed Khan <wahidXX@xxx.com>
2026-01-29 05:06:59 +0300

    Added react form hook and react query

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,6 @@
+import { useFormik } from "formik";
 import { useState } from "react";
+import * as Yup from 'yup';
 
 export default function Register() {
 
@@ -6,58 +8,82 @@ export default function Register() {
         email: "", password: "", role: "user",  cnfPassword: ""
     });
 
-    const [error, setError] = useState("");
-    const handleChange = (e) => {
-        setUser({ ...user, [e.target.name]: e.target.value });
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(user);
-        
-        // Registration logic here
-    }
+    const RegisterForm = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+            cnfPassword: '',
+            role: 'user'
+        },
+        onSubmit: values => {
+            console.log('Form data', values);
+        },
+        validationSchema : Yup.object({
+            email: Yup.string().email('Invalid email address').required('Email is required'),
+            password: Yup.string().min(6, 'Password must be at least 6 characters').required('Password is required'),
+            cnfPassword: Yup.string().oneOf([Yup.ref('password'), null], 'Passwords must match').required('Confirm Password is required'),
+            role: Yup.string().required('Role is required'),
+        }),
+    })
 
     return (
      <div className="mt-30 p-6 shadow mt-6 text-gray-800 bg-white 
      rounded text-center w-96 mx-auto">
             <h2 className="text-2xl font-bold mb-4 text-center">Register</h2>
 
-            {error && <p className="text-red-600 mb-2">{error}</p>}
-
-            <form onSubmit={handleSubmit} className="space-y-4">
+            <form onSubmit={RegisterForm.handleSubmit} className="space-y-4">
                 <input
-                    type="email"
+                    type="email" id="email" name="email"
                     placeholder="Email"
                     className="w-full border p-2 rounded"
-                    onChange={handleChange}
-                    name="email"
-                    required
+                    onChange={RegisterForm.handleChange}
+                     onBlur={RegisterForm.handleBlur}
+                     value={RegisterForm.values.email}
                 />
+                 {RegisterForm.touched.email && RegisterForm.errors.email && (
+                    <p className="text-red-500 text-xs text-left">
+                        {RegisterForm.errors.email}
+                    </p>
+                )}
                 <input
-                    type="password"
+                    type="password" id="password" name="password"
                     placeholder="Password"
                     className="w-full border p-2 rounded"
-                     onChange={handleChange}
-                    name="password"
-                    required
+                     onChange={RegisterForm.handleChange}
+                      onBlur={RegisterForm.handleBlur}
+                      value={RegisterForm.values.password}
                 />
+                {RegisterForm.touched.password && RegisterForm.errors.password && (
+                    <p className="text-red-500 text-xs text-left">
+                        {RegisterForm.errors.password}
+                    </p>
+                )}
                 <input
-                    type="password"
+                    type="password" id="cnfPassword" name="cnfPassword"
                     placeholder="Confirm Password"
                     className="w-full border p-2 rounded"
-                     onChange={handleChange}
-                    name="cnfPassword"
-                    required
+                     onChange={RegisterForm.handleChange}
+                      onBlur={RegisterForm.handleBlur}
+                      value={RegisterForm.values.cnfPassword}
                 />
+                 {RegisterForm.touched.cnfPassword && RegisterForm.errors.cnfPassword && (
+                    <p className="text-red-500 text-xs text-left">
+                        {RegisterForm.errors.cnfPassword}
+                    </p>
+                )}
                 <input
-                    type="role"
+                    type="role" id="role" name="role"
                     placeholder="Role"
                     className="w-full border p-2 rounded"
-                     onChange={handleChange}
-                    name="role"
-                    required
+                    onChange={RegisterForm.handleChange}
+                     onBlur={RegisterForm.handleBlur}
+                    value={RegisterForm.values.role}
                 />
+                  {RegisterForm.touched.role && RegisterForm.errors.role && (
+                    <p className="text-red-500 text-xs text-left">
+                        {RegisterForm.errors.role}
+                    </p>
+                )}
 
                 <button
                     type="submit"
